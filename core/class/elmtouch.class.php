4f5440fa70e6757ece2e31720c972213a53fcef7
Jean-Michel Vedrine <vedrine@vedrine.org>
2018-02-07 10:34:50 +0100

    First commit

---
+++ core/class/elmtouch.class.php
@@ -0,0 +1,162 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class elmtouch extends eqLogic {
+    /*     * *************************Attributs****************************** */
+
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    public static function dependancy_info() {
+        $return = array();
+        $return['log'] = 'elmtouch_update';
+        $return['progress_file'] = jeedom::getTmpFolder('elmtouch') . '/dependance';
+        $return['state'] = 'ok';
+        // TODO détecter si nefit easy server est installé.
+        return $return;
+    }
+
+    public static function dependancy_install() {
+        log::remove(__CLASS__ . '_update');
+        return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh ' . jeedom::getTmpFolder('elmtouch') . '/dependance', 'log' => log::getPathToLog(__CLASS__ . '_update'));
+    }
+
+    public static function start() {
+        self::cron15();
+    }
+    
+    /*
+     * Fonction exécutée automatiquement toutes les minutes par Jeedom
+      public static function cron() {
+
+      }
+     */
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les heures par Jeedom
+      public static function cronHourly() {
+
+      }
+     */
+
+    /*
+     * Fonction exécutée automatiquement tous les jours par Jeedom
+      public static function cronDaily() {
+
+      }
+     */
+
+    public static function cron15() {
+        foreach (self::byType('elmtouch') as $eqLogic) {
+            if ($eqLogic->getConfiguration('enable') == 1 && !$eqLogic->getState()) {
+                // TODO
+            }
+            if ($eqLogic->getConfiguration('enable') == 0 && $eqLogic->getState()) {
+                // DODO
+            }
+        }
+    }
+
+
+
+    /*     * *********************Méthodes d'instance************************* */
+
+    public function preInsert() {
+        
+    }
+
+    public function postInsert() {
+        
+    }
+
+    public function preSave() {
+        
+    }
+
+    public function postSave() {
+        
+    }
+
+    public function preUpdate() {
+        
+    }
+
+    public function postUpdate() {
+        
+    }
+
+    public function preRemove() {
+        
+    }
+
+    public function postRemove() {
+        
+    }
+
+    /*
+     * Non obligatoire mais permet de modifier l'affichage du widget si vous en avez besoin
+      public function toHtml($_version = 'dashboard') {
+
+      }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action après modification de variable de configuration
+    public static function postConfig_<Variable>() {
+    }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action avant modification de variable de configuration
+    public static function preConfig_<Variable>() {
+    }
+     */
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+class elmtouchCmd extends cmd {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    /*     * *********************Methode d'instance************************* */
+
+    /*
+     * Non obligatoire permet de demander de ne pas supprimer les commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+      public function dontRemoveCmd() {
+      return true;
+      }
+     */
+
+    public function execute($_options = array()) {
+        
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+

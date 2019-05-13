d8f391190e6a39e6353900b9d698ac2da58d97c8
Jean-Michel Vedrine <vedrine@vedrine.org>
2019-05-13 17:42:02 +0200

    Update Font Awesome for Jeedom v4

--- desktop/php/elmtouch.php
+++ desktop/php/elmtouch.php
@@ -11,7 +11,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
     <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un ELM Touch}}</a>
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fas fa-plus-circle"></i> {{Ajouter un ELM Touch}}</a>
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
 <?php
 foreach ($eqLogics as $eqLogic) {
@@ -25,24 +25,24 @@ foreach ($eqLogics as $eqLogic) {
 
    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
     <legend>{{Mes ELM Touchs}}</legend>
-  <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+  <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
   <div class="eqLogicThumbnailContainer">
 <?php
 if (count($eqLogics) == 0) {
       echo '<div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-          <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
+          <i class="fas fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
         <br>
         <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
     </div>';
 }
 ?>
       <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-      <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+      <i class="fas fa-wrench" style="font-size : 6em;color:#767676;"></i>
     <br>
     <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
   </div>
   </div>
-  <legend><i class="fa fa-table"></i> {{Mon ELM Touch}}</legend>
+  <legend><i class="fas fa-table"></i> {{Mon ELM Touch}}</legend>
 <div class="eqLogicThumbnailContainer">
     <?php
 foreach ($eqLogics as $eqLogic) {
@@ -58,19 +58,19 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-    <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+    <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a>
+  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a>
   <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
 <?php
 try {
 	$plugin = plugin::byId('calendar');
 	if (is_object($plugin)) {
 ?>
-    <li  role="presentation"><a href="#configureSchedule" aria-controls="profile" data-toggle="tab"><i class="fa fa-clock-o"></i> {{Programmation}}</a></li>
+    <li  role="presentation"><a href="#configureSchedule" aria-controls="profile" data-toggle="tab"><i class="fas fa-clock-o"></i> {{Programmation}}</a></li>
 <?php
 }
 } catch (Exception $e) {
@@ -125,7 +125,7 @@ foreach (object::all() as $object) {
         <div class="form-group">
         <label class="col-sm-3 control-label">{{Coefficient de conversion}}
             <sup>
-                <i class="fa fa-question-circle tooltips" title="{{Un m3 de gaz possède un pouvoir calorifique variable. GRDF calcule un coefficient de conversion m3 -> kWh suivant la ville et la période. L'application Bosch utilise 8.125.}}"></i>
+                <i class="fas fa-question-circle tooltips" title="{{Un m3 de gaz possède un pouvoir calorifique variable. GRDF calcule un coefficient de conversion m3 -> kWh suivant la ville et la période. L'application Bosch utilise 8.125.}}"></i>
             </sup>
         </label>
         <div class="col-sm-3">
@@ -135,14 +135,14 @@ foreach (object::all() as $object) {
     <div class="form-group">
         <label class="col-sm-3 control-label">{{Prix du gaz par kWh}}
             <sup>
-                <i class="fa fa-question-circle tooltips" title="{{Le gaz en France est facturé par kWh même si le compeur affiche des m3, consultez votre facture pour connaître ce prix.}}"></i>
+                <i class="fas fa-question-circle tooltips" title="{{Le gaz en France est facturé par kWh même si le compeur affiche des m3, consultez votre facture pour connaître ce prix.}}"></i>
             </sup>
         </label>
         <div class="col-sm-3">
             <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="prixgazkwh" placeholder="{{Prix en € par kWh}}"/>
         </div>
         <div class="col-lg-2">
-              <a class="btn btn-info bt_reImport" id="bt_reImport"><i class='fa fa-qrcode'></i> {{Ré-importer les consommations}}</a>
+              <a class="btn btn-info bt_reImport" id="bt_reImport"><i class='fas fa-qrcode'></i> {{Ré-importer les consommations}}</a>
         </div>
     </div>
     <div class="form-group">
@@ -157,7 +157,7 @@ foreach (object::all() as $object) {
             <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Auto-actualisation (cron)}}"/>
         </div>
         <div class="col-sm-1">
-            <i class="fa fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
+            <i class="fas fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
         </div>
     </div>
 </fieldset>
@@ -165,7 +165,7 @@ foreach (object::all() as $object) {
 </div>
 
 <div role="tabpanel" class="tab-pane" id="commandtab">
-<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
+<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fas fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
 <table id="table_cmd" class="table table-bordered table-condensed">
     <thead>
         <tr>

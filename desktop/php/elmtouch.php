6146d0a308897109beea07b3c838eed1673db301
Jean-Michel Vedrine <vedrine@vedrine.org>
2019-06-04 18:58:39 +0200

    Fix FR lang json file

--- desktop/php/elmtouch.php
+++ desktop/php/elmtouch.php
@@ -8,49 +8,34 @@ $eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2 col-md-3 col-sm-4">
-        <div class="bs-sidebar">
-            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fas fa-plus-circle"></i> {{Ajouter un ELM Touch}}</a>
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-<?php
-foreach ($eqLogics as $eqLogic) {
-    $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity .'"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-}
-            ?>
-           </ul>
-       </div>
-   </div>
-
-   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-    <legend>{{Mes ELM Touchs}}</legend>
+  <div class="col-xs-12 eqLogicThumbnailDisplay">
+  <legend>{{Mon ELM Touch}}</legend>
   <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
   <div class="eqLogicThumbnailContainer">
 <?php
 if (count($eqLogics) == 0) {
-      echo '<div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-          <i class="fas fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
-        <br>
-        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
-    </div>';
+      echo '<div class="cursor eqLogicAction logoPrimary" data-action="add">
+           <i class="fas fa-plus-circle"></i>
+           <br>
+          <span>{{Ajouter}}</span>
+          </div>';
 }
 ?>
-      <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-      <i class="fas fa-wrench" style="font-size : 6em;color:#767676;"></i>
+      <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+      <i class="fas fa-wrench"></i>
     <br>
-    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+    <span>{{Configuration}}</span>
   </div>
   </div>
   <legend><i class="fas fa-table"></i> {{Mon ELM Touch}}</legend>
 <div class="eqLogicThumbnailContainer">
     <?php
 foreach ($eqLogics as $eqLogic) {
-    $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-    echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
-    echo "<br>";
-    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
+    $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+    echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+    echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+    echo '<br>';
+    echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
     echo '</div>';
 }
 ?>
@@ -67,10 +52,10 @@ foreach ($eqLogics as $eqLogic) {
     <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
 <?php
 try {
-	$plugin = plugin::byId('calendar');
-	if (is_object($plugin)) {
+    $plugin = plugin::byId('calendar');
+    if (is_object($plugin)) {
 ?>
-    <li  role="presentation"><a href="#configureSchedule" aria-controls="profile" data-toggle="tab"><i class="fas fa-clock-o"></i> {{Programmation}}</a></li>
+    <li  role="presentation"><a href="#configureSchedule" aria-controls="profile" data-toggle="tab"><i class="fas fa-clock"></i> {{Programmation}}</a></li>
 <?php
 }
 } catch (Exception $e) {

a1fee2be0279b5bcb9e829678d9ce025cf20784a
Jean-Michel Vedrine <vedrine@vedrine.net>
2022-06-01 22:18:19 +0200

    Install nodejs v16

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -7,7 +7,7 @@ PLUGIN=$(basename "$(realpath $BASEDIR/..)")
 ##################################################################
 wget https://raw.githubusercontent.com/NebzHB/nodejs_install/main/install_nodejs.sh -O $BASEDIR/install_nodejs.sh &>/dev/null
 
-installVer='14' 	#NodeJS major version to be installed
+installVer='16' 	#NodeJS major version to be installed
 
 pre
 step 0 "Vérification des droits"
